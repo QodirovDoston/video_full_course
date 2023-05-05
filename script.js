d7bdadb40cc552d13be284091e0fd1f86141d591
qodirovdoston806 <qodirovdoston806@gmail.com>
2023-05-05 16:12:43 +0500

    ddf

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("hello world");
+const App={
+    $:{
+        menu: document.querySelector('[data-id="menu"]'),
+        itemsMenu: document.querySelector('[data-id="menu-items"]'),
+        resetBtn: document.querySelector('[data-id="reset-btn"]'),
+        newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
+        squares: document.querySelectorAll('[data-id="square"]')
+    },
+
+init(){
+
+    App.registerEventListeners();
+},
+registerEventListeners(){
+// Done
+    App.$.menu .addEventListener('click', (event)=>{
+        App.$. itemsMenu.classList.toggle('hidden');
+    });
+    // Todo
+    App.$.resetBtn.addEventListener('click',event =>{
+        console.log("reset the game");
+    }),
+    // Todo
+
+    App.$.newRoundBtn.addEventListener('click',event =>{
+        console.log("new round the game");
+    });
+    // Todo
+
+    App.$.squares.forEach(squares=>{
+        squares.addEventListener('click', event=>{
+            console.log(`square with id ${event.target.id} was clicked`);
+        })
+    })
+
+}
+}
+window.addEventListener("load", App.init); 
